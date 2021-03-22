181199f914b1f634e48e66b6892cafa5095ab45f
kangyao.ma@sap.com <kangyao.ma@sap.com>
2021-03-22 11:08:36 +0800

    init project

---
+++ src/main/java/com/example/kafkademo/KafkademoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example.kafkademo.controller")
+public class KafkademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkademoApplication.class, args);
+	}
+
+}
